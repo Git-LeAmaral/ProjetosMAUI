8f62d3476b9be02756efc392dbb8c41a6f2d2c27
Leandro Amaral <lramaral02m@gmail.com>
2025-04-28 20:15:01 -0300

    Finalizando funcionamento do app no windows

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+
 namespace AppNumeroDaSorte;
 
 public partial class MainPage : ContentPage
@@ -11,6 +13,34 @@ public partial class MainPage : ContentPage
 	{
 		NameApp.IsVisible = false;
 		ContainerLuckNumbers.IsVisible = true;
+
+		// Fazendo a atribuição dos numeros
+		var list = GenerateLuckNumbers();
+
+		LuckNumber01.Text = list.ElementAt(0).ToString("D2");
+		LuckNumber02.Text = list.ElementAt(1).ToString("D2");
+		LuckNumber03.Text = list.ElementAt(2).ToString("D2");
+		LuckNumber04.Text = list.ElementAt(3).ToString("D2");
+		LuckNumber05.Text = list.ElementAt(4).ToString("D2");
+		LuckNumber06.Text = list.ElementAt(5).ToString("D2");
+	}
+
+	// Método de geranção de números randomicos
+	private SortedSet<int> GenerateLuckNumbers()
+	{
+		// Usar o HashSet para não repetir os números
+		var list = new SortedSet<int>();
+
+		while(list.Count < 6)
+		{
+            var random = new Random();
+            var luckNumber = random.Next(1, 60);
+
+			list.Add(luckNumber);
+        }
+
+		return list;
+		
 	}
 
 }
